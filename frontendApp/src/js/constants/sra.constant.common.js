fa0bcc63b0919d2fa21748624763fa729c598118
hxghxg527 <hxghxg527@163.com>
2016-10-22 23:46:46 +0800

    update app name

--- frontendApp/src/js/constants/sra.constant.common.js
+++ frontendApp/src/js/constants/sra.constant.common.js
@@ -1,6 +1,6 @@
 angular.module('sra.constant.common', [])
     .constant('SRA_CONSTANT_COMMON', {
-        APP_NAME: "Stock Record And Analysis",
+        APP_NAME: "Stock Record Analysis System",
         EVENT_TYPES: {
             PV_CHANGE_LOGIN_ROUTER_MODELS: "pv_change_login_router_models"
         },
